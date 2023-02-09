42478aba4b3b36debce6566f7dcba9ad1bba0b17
Heja <74791506+schart@users.noreply.github.com>
2023-02-10 01:26:10 +0300

    Go connection on ApacheKafka

--- broker/kafka_producer.go
+++ broker/kafka_producer.go
@@ -7,12 +7,37 @@ import (
 	"github.com/Shopify/sarama"
 )
 
-func NewProducer() (sarama.SyncProducer, error) {
-	brokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
+// Go üzerinde hiç kullanmadım lakin internet yardımı ile ortaya bir şeyler çıktı
+// Kod okunabilirlik için kısaltılmıştır ve error gibi fonksiyonları utils adında bir dosya da tutmak okunabilirliği arttırmak ile beraber projeyi hızlandırır.
 
-	// We need to change below configs when kafka cluster was created
-	// We don't know how we can connect to kafka
-	config := sarama.NewConfig()
+config := sarama.NewConfig()
+config.Version = sarama.V2_5_0  
 
-	return sarama.NewSyncProducer(brokers, config)
+brokers := []string{"localhost:9092"}  // Default port
+
+client, err := sarama.NewClient(brokers, config) // Hata olmadığı sürece dosyalar arası client objesi sorunsuz çalışabilir ve tekrar statement oluşturmayız: "new"
+if err != nil { panic(err) }
+
+defer func() { 
+	     if err := client.Close(); 
+	      err != nil { panic(err) } 
+	     }()
+
+
+// Producer aşağıda ki gibidir ve consumer aracılığı ile okunabilir.
+// Başka bir dosyaya alınabilir örn: router'a dayatılabilir.
+
+producer, err := sarama.NewSyncProducerFromClient(client)
+
+if err != nil { panic(err) }
+defer func() { if err := producer.Close(); err != nil { panic(err) } }()
+
+message := &sarama.ProducerMessage{
+    Topic: "test-topic",
+    Value: sarama.StringEncoder("test message"),
 }
+
+partition, offset, err := producer.SendMessage(message)
+if err != nil { panic(err) }
+
+fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
